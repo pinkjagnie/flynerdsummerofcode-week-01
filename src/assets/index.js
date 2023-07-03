4553c374a87003bbd718f9cdc032ef04496a3879
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-03 23:55:35 +0200

    Added Curie's images

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+import curie01Transparent from "./curie-01_500-500.png";
+import curie02Transparent from "./curie-02_500-500.png";
+import curie03Transparent from "./curie-03_500-500.png";
+
+import curie01Big from "./curie-01_1200-1200.jpg";
+import curie02Big from "./curie-02_1200-1200.jpg";
+import curie03Big from "./curie-03_1200-1200.jpg";
+
+export {
+  curie01Transparent,
+  curie02Transparent,
+  curie03Transparent,
+  curie01Big,
+  curie02Big,
+  curie03Big
+}
