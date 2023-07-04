b5f7fbeb4ad70a09b7d1ea58253f66a0af8320d1
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-04 20:50:09 +0200

    Translation: done world war I page

--- src/components/content/life/WorldWarIContent.js
+++ src/components/content/life/WorldWarIContent.js
@@ -5,7 +5,7 @@ import animationData from "../../../assets/lotties/chemistry-class.json";
 
 import styles from "@/styles/styles";
 
-const WorldWarIContent = () => {
+const WorldWarIContent = ({ translate }) => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -20,12 +20,12 @@ const WorldWarIContent = () => {
       <div className={styles.basicTopPartOfPage}>
         <Lottie options={defaultOptions} height={250} width={250} />
         <div className="lg:w-[90%]">
-          <p className={styles.basicParagraph}><em className="text-[#d66382] font-bold uppercase">Gdy 1 sierpnia 1914 </em> wybuchła I wojna światowa, siedemnastoletnia Irène i dziesięcioletnia Ève przebywały z dala od Paryża i matki. Były na wakacjach w l’Arcouest pod opieką przyjaciół Marii. Maria Skłodowska-Curie pozostała w Paryżu, by strzec Instytutu Radowego i próbki radu.</p>
+          <p className={styles.basicParagraph}><em className="text-[#d66382] font-bold uppercase">{translate('When August 1, 1914')} </em> {translate('world war short')}</p>
         </div>
       </div>
 
       <div>
-        <p className={styles.basicParagraph}>Rząd ogłosił, że rad znajdujący się w Instytucie Radowym stanowi dobro narodowe i należy go ochronić. Uczona wywiozła rad na czas wojny do Bordeaux. Maria nie mogąc służyć Polsce, postanowiła służyć Francji. Zebrała aparaty rentgenowskie z paryskich pracowni i zorganizowała specjalne samochody ciężarowe z aparaturą, tzw. „małe Curie” (w lipcu 1916, jako jedna z pierwszych kobiet, uzyskała prawo jazdy na ciężarówki, by móc te samochody prowadzić). Były to pionierskie działania z zakresu diagnostyki zdrowia. Dzięki jej pracy i wytrwałości można było wykonywać zdjęcia rentgenowskie w polowych warunkach. Marii towarzyszyli lekarze wojskowi i Irène, która razem z matką szkoliła techników radiologów. Ta działalność uratowała wielu żołnierzy od pewnej śmierci. Była także członkinią Szwajcarskiego Komitetu Generalnego Pomocy Ofiarom Wojny w Polsce. </p>
+        <p className={styles.basicParagraph}>{translate('world war paragraph')} </p>
       </div>
     </>
   )
--- src/pages/life/world-war-i.js
+++ src/pages/life/world-war-i.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+import { useRouter } from 'next/router';
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 import WorldWarIContent from "@/components/content/life/WorldWarIContent";
 import GoHomeLink from "@/components/GoHomeLink";
 
 import styles from "@/styles/styles";
 
 const worldWarIPage = () => {
+  const { locale, locales, asPath } = useRouter();
+
+	const { t: translate } = useTranslation('world-war-i');
+
   return(
     <section className={`${styles.sectionPaddingTop} min-h-screen`}>
-      <WorldWarIContent />
+      <WorldWarIContent translate={translate} />
 
       {/* go home */}
       <GoHomeLink />
@@ -18,3 +26,11 @@ const worldWarIPage = () => {
 };
 
 export default worldWarIPage;
+
+export async function getStaticProps({ locale }) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['world-war-i'])),
+		},
+	}
+};
