97f22ede578ea775501f11d00fe7d8736bd8666b
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-04 18:32:16 +0200

    Translation: done early years page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { useRouter } from "next/router";
 
 import Links from "./Links/Links";
 
--- src/pages/life/early-years.js
+++ src/pages/life/early-years.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+import { useRouter } from 'next/router';
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 import EarlyYearsContent from "@/components/content/life/EarlyYearsContent";
 import GoHomeLink from "@/components/GoHomeLink";
 
 import styles from "@/styles/styles";
 
 const earlyYearsPage = () => {
+  const { locale, locales, asPath } = useRouter();
+
+	const { t: translate } = useTranslation('early-years');
+
   return(
     <section className={`${styles.sectionPaddingTop} min-h-screen`}>
-      <EarlyYearsContent />
+      <EarlyYearsContent translate={translate} />
 
       {/* go home */}
       <GoHomeLink />
@@ -18,3 +26,11 @@ const earlyYearsPage = () => {
 };
 
 export default earlyYearsPage;
+
+export async function getStaticProps({ locale }) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['early-years'])),
+		},
+	}
+};
