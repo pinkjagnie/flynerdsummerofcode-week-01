6c7d2ecb258a8d69cb5890b7b5eab1b06f193d62
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-04 20:55:48 +0200

    Translation: done postwar years page

--- src/pages/life/postwar-years.js
+++ src/pages/life/postwar-years.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+import { useRouter } from 'next/router';
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 import PostwarYearsContent from "@/components/content/life/PostwarYearsContent";
 import GoHomeLink from "@/components/GoHomeLink";
 
 import styles from "@/styles/styles";
 
 const postwarYearsPage = () => {
+  const { locale, locales, asPath } = useRouter();
+
+	const { t: translate } = useTranslation('postwar-years');
+
   return(
     <section className={`${styles.sectionPaddingTop} min-h-screen`}>
-      <PostwarYearsContent />  
+      <PostwarYearsContent translate={translate} />  
 
       {/* go home */}
       <GoHomeLink />
@@ -17,3 +25,11 @@ const postwarYearsPage = () => {
 };
 
 export default postwarYearsPage;
+
+export async function getStaticProps({ locale }) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['postwar-years'])),
+		},
+	}
+};
