9cdc903edddae2cf07f79da35b4a40ed3c96f478
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-04 20:45:01 +0200

    Translation: done nobel prizes page

--- src/pages/life/nobel-prizes.js
+++ src/pages/life/nobel-prizes.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+import { useRouter } from 'next/router';
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 import NobelPrizesContent from "@/components/content/life/NobelPrizesContent";
 import GoHomeLink from "@/components/GoHomeLink";
 
 import styles from "@/styles/styles";
 
 const nobelPrizesPage = () => {
+  const { locale, locales, asPath } = useRouter();
+
+	const { t: translate } = useTranslation('nobel-prizes');
+
   return(
     <section className={`${styles.sectionPaddingTop} min-h-screen`}>
-      <NobelPrizesContent />
+      <NobelPrizesContent translate={translate} />
 
       {/* go home */}
       <GoHomeLink />
@@ -18,3 +26,11 @@ const nobelPrizesPage = () => {
 };
 
 export default nobelPrizesPage;
+
+export async function getStaticProps({ locale }) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['nobel-prizes'])),
+		},
+	}
+};
