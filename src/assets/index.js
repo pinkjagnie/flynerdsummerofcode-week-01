9a5a3e450a63475ff1085e2a64e1d23aadc1747e
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-04 13:45:53 +0200

    Clean up in assets folder

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,11 @@
-import curie01Transparent from "./curie-01_500-500.png";
-import curie02Transparent from "./curie-02_500-500.png";
-import curie03Transparent from "./curie-03_500-500.png";
+import curie01Transparent from "./images/curie-01_500-500.png";
+import curie02Transparent from "./images/curie-02_500-500.png";
+import curie03Transparent from "./images/curie-03_500-500.png";
 
-import curie01Big from "./curie-01_1200-1200.jpg";
-import curie02Big from "./curie-02_1200-1200.jpg";
-import curie03Big from "./curie-03_1200-1200.jpg";
-import curie04Big from "./curie-04_1200-1200.jpg";
+import curie01Big from "./images/curie-01_1200-1200.jpg";
+import curie02Big from "./images/curie-02_1200-1200.jpg";
+import curie03Big from "./images/curie-03_1200-1200.jpg";
+import curie04Big from "./images/curie-04_1200-1200.jpg";
 
 export {
   curie01Transparent,
