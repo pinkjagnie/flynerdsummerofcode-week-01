3e31d6915de8c449ef777091f8ae902c07d45e09
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-04 20:35:24 +0200

    Translation: done new elements page

--- src/pages/life/new-elements.js
+++ src/pages/life/new-elements.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+import { useRouter } from 'next/router';
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 import NewElementsContent from "@/components/content/life/NewElementsContent";
 import GoHomeLink from "@/components/GoHomeLink";
 
 import styles from "@/styles/styles";
 
 const newElementsPage = () => {
+  const { locale, locales, asPath } = useRouter();
+
+	const { t: translate } = useTranslation('new-elements');
+
   return(
     <section className={`${styles.sectionPaddingTop} min-h-screen`}>
-      <NewElementsContent />
+      <NewElementsContent translate={translate} />
 
       {/* go home */}
       <GoHomeLink />
@@ -18,3 +26,11 @@ const newElementsPage = () => {
 };
 
 export default newElementsPage;
+
+export async function getStaticProps({ locale }) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['new-elements'])),
+		},
+	}
+};
