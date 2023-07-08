d729daa682aab7623be90b4d42f846f46f87a8b4
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-08 13:55:02 +0200

    Added file with quizQuestions

---
+++ src/constans/quizQuestions.js
@@ -0,0 +1,41 @@
+export const quizQuestions = {
+  totalQuestions: 5,
+  questions: [
+    {
+      id: 1,
+      question: "W którym roku urodziła się Maria Skłodowska-Curie?",
+      answers: ["1888", "1867", "1840", "1853"],
+      correctAnswer: "1867",
+    },
+    {
+      id: 2,
+      question: "Ile Nagród Nobla otrzymała Maria Skłodowska-Curie?",
+      answers: ["3", "2", "1", "4"],
+      correctAnswer: "2",
+    },
+    {
+      id: 3,
+      question: "Mężem Marii Skłodowskiej-Curie był",
+      answers: ["Jacques Curie", "Jan Curie", "Pierre Curie", "Paul Curie"],
+      correctAnswer: "Pierre Curie",
+    },
+    {
+      id: 4,
+      question: "Maria Skłodowska-Curie stworzyła:",
+      answers: ["promieniotwórczość", "radiochemię", "chemię", "radiację"],
+      correctAnswer: "radiochemię",
+    },
+    {
+      id: 5,
+      question:
+        "Jak nazywają się pierwiastki, które odryła Maria Skłodowska-Curie?",
+      answers: [
+        "frans i cez",
+        "rad i polon",
+        "kopernik i promet",
+        "kiur i einstein",
+      ],
+      correctAnswer: "rad i polon",
+    },
+  ],
+};
